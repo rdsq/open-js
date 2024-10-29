4246e0f173e6fea4264b57a7df86bb2c8966f405
rdsq <62446963+rdsq@users.noreply.github.com>
2024-10-29 14:25:44 +0200

    fmt

--- mod.ts
+++ mod.ts
@@ -7,7 +7,7 @@ import { spawn } from 'node:child_process';
  */
 export default function open(resource: string): Promise<void> {
     const command = createNativeCommand(resource);
-    return new Promise(resolve => {
+    return new Promise((resolve) => {
         const cmd = spawn(command.command, command.args, {
             stdio: 'inherit',
         });
